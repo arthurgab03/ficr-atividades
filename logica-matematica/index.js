69038f2b9126b645f5a75bdb12fb76eb559d81db
Arthur Gabriel <arthurgabriel120703@gmail.com>
2023-06-14 23:26:12 -0300

    first commit

---
+++ logica-matematica/index.js
@@ -0,0 +1,22 @@
+// Trabalho para o segundo ciclo avaliati
+
+let saldoInicial = parseFloat(prompt('Insira o seu saldo'));
+
+const sorvete1 = 'a';
+const sorvete2 = 'b';
+const sorvete3 = 'c';
+
+let cont = 0;
+
+
+do { let mensagemInicial = (prompt('Seja bem vindo à Sorveteria FICR! Temos vários sabores de sorvete disponiveis, qual sabor você deseja comprar?\n Preços: \n Sorvete de Baunilha: 2.50 \n Sorvete de Menta: 3.45 \n Sorvete azul: 5.58. \n OBS: Para escolher um sorvete, basta utilizar o código do sorvete (são respectivamente a, b e c).'))
+
+if (mensagemInicial == sorvete1) {
+    alert('funcionou');
+    cont++;
+} 
+
+
+ } while (cont = 3);
+
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Atividades do primeiro período de Análise e Desenvolvimento de Sistemas na Faculdade Católica Imaculada Conceição do Recife.
+
+## Links importantes:
+
+- - [CLIQUE AQUI PARA VISUALIZAR A ATIVIDADE DE FORMULÁRIOS](https://arthurgab03.github.io/ficr-atividades/html-css/atv4/index.html)
